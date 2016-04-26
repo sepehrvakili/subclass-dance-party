c87dd2c5fd1aaae6698646a4ab6cc6161e7c4f8c
sepehrvakili <sdvakili@gmail.com>
2016-04-25 19:36:01 -0700

    Implement line up button

--- src/init.js
+++ src/init.js
@@ -1,32 +1,83 @@
 $(document).ready(function() {
   window.dancers = [];
 
-  $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
+  // var addDancerButton = function(cssClassButton, cssClassDancer, dataName) {
+  //   $(cssClassButton).on('click'), function(event) {
+  //     var dancerFuncName = $(this).data(dataName);
+  //     var dancerFunc = window[dancerFuncName];
 
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+  //     var dancer = new dancerFunc(
+  //       $('body').height() * Math.random(),
+  //       $('body').width() * Math.random(),
+  //       Math.random() * 1000
+  //       );
+
+  //     $('body').append(dancer.$node.addClass(cssClassDancer));
+  //   }; 
+  // };
+
+  // addDancerButton('.addBlinkyDancerButton', 'blinky', 'blinky-dancer-maker-function-name');
+  // addDancerButton('.addBouncyDancerButton', 'bouncy', 'bouncy-dancer-maker-function-name');
+  // addDancerButton('.addSlidyDancerButton', 'slidy', 'slidy-dancer-maker-function-name');
+
+
+  $('.addBlinkyDancerButton').on('click', function(event) {
+    var blinkyDancerMakerFunctionName = $(this).data('blinky-dancer-maker-function-name');
+    var blinkyDancerMakerFunction = window[blinkyDancerMakerFunctionName];
+
     // make a dancer with a random position
-
-    var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+    var blinkyDancer = new blinkyDancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
-    console.log(dancer, '<-- Dancer');
-    $('body').append(dancer.$node);
+    window.dancers.push(blinkyDancer);
+    $('body').append(blinkyDancer.$node.addClass('blinky'));
+  });
+
+  $('.addBouncyDancerButton').on('click', function(event) {
+    var bouncyDancerMakerFunctionName = $(this).data('bouncy-dancer-maker-function-name');
+    var bouncyDancerMakerFunction = window[bouncyDancerMakerFunctionName];
+
+    // make a dancer with a random position
+    console.log(bouncyDancerMakerFunction, bouncyDancerMakerFunctionName);
+    var bouncyDancer = new bouncyDancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
+      Math.random() * 1000
+    );
+    window.dancers.push(bouncyDancer);
+    $('body').append(bouncyDancer.$node.addClass('bouncy'));
+  });
+
+  $('.addSlidyDancerButton').on('click', function(event) {
+    var slidyDancerMakerFunctionName = $(this).data('slidy-dancer-maker-function-name');
+    var slidyDancerMakerFunction = window[slidyDancerMakerFunctionName];
+
+    // make a dancer with a random position
+    console.log(slidyDancerMakerFunction, slidyDancerMakerFunctionName);
+    var slidyDancer = new slidyDancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
+      Math.random() * 1000
+    );
+    window.dancers.push(slidyDancer);
+    $('body').append(slidyDancer.$node.addClass('slidy'));
+  });
+
+  $('.lineUpButton').on('click', function(e) {
+    var top = 100;
+    window.dancers.forEach(function(dancer) {
+      top += 50;
+      dancer.lineUp(top, 150);
+    });
   });
 });
+
+
+
+
+
+
+
 
